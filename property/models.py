435c1a10e5957bd3aff28057864243cb6dbfe33d
kaser137 <107580630+kaser137@users.noreply.github.com>
2023-03-12 07:27:17 +0200

    create owners from Flat

--- property/models.py
+++ property/models.py
@@ -73,7 +73,7 @@ class Complaint(models.Model):
 
 class Owner(models.Model):
     owner = models.CharField('ФИО владельца', max_length=200)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
+    owners_phonenumber = models.CharField('Номер владельца', max_length=20, null=True)
     owner_pure_phone = PhoneNumberField(verbose_name='Нормализованный номер владельца', null=True, blank=True)
     flat = models.ManyToManyField(Flat, verbose_name="Квартиры в собственности", related_name='owners')
 
