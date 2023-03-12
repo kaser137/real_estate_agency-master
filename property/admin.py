bbe542c3e989733838c65c77cf9d5d62d10d8b70
kaser137 <107580630+kaser137@users.noreply.github.com>
2023-03-12 12:30:06 +0200

    delete fields in Flat

--- property/admin.py
+++ property/admin.py
@@ -5,28 +5,28 @@ from .models import Flat, Complaint, Owner
 
 class FlatInline(admin.TabularInline):
     model = Owner.flat.through
-    raw_id_fields = ["owner",]
+    raw_id_fields = ["owner", "flat"]
     extra = 1
     verbose_name = 'Собственник'
     verbose_name_plural = 'Собственники'
 
 class FlatAdmin(admin.ModelAdmin):
     inlines = [FlatInline,]
-    search_fields = ('town', 'address', 'owner', 'id')
-    list_display = ('address', 'price', 'new_building', 'construction_year', 'town', 'owner_pure_phone')
+    search_fields = ('town', 'address', 'id')
+    list_display = ('address', 'price', 'new_building', 'construction_year', 'town', )
     list_editable = ('new_building',)
     readonly_fields = ('created_at',)
     list_filter = ('new_building', 'rooms_number', 'floor', 'has_balcony')
     raw_id_fields = ('likes', )
 
 class ComplaintAdmin(admin.ModelAdmin):
-    list_display = ('text',)
+    list_display = ('user', 'flat', 'text', )
     raw_id_fields = ('user', 'flat')
 
 
 class OwnerAdmin(admin.ModelAdmin):
-
     raw_id_fields = ('flat', )
+
 
 
 
