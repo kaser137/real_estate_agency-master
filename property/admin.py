c083e725ad79ef45c0fc3b0a726cf23e28f027e8
kaser137 <107580630+kaser137@users.noreply.github.com>
2023-03-14 00:36:27 +0200

    Add info about owners to the page Flat.

--- property/admin.py
+++ property/admin.py
@@ -2,7 +2,18 @@ from django.contrib import admin
 
 from .models import Flat, Complaint, Owner
 
+
+class FlatInline(admin.TabularInline):
+    model = Owner.flat.through
+    raw_id_fields = ["owner", "flat"]
+    extra = 1
+    verbose_name = 'Собственник'
+    verbose_name_plural = 'Собственники'
+
+
+
 class FlatAdmin(admin.ModelAdmin):
+    inlines = [FlatInline, ]
     search_fields = ('town', 'address', 'id', 'owner')
     list_display = ('address', 'price', 'new_building', 'construction_year', 'town',)
     list_editable = ('new_building',)
