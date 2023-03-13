2fa38e68d55fa4a55af35680930780363884dbbb
kaser137 <107580630+kaser137@users.noreply.github.com>
2023-03-13 23:17:21 +0200

    add field Flat.owner_pure_phone.

--- property/models.py
+++ property/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
+    owner = models.CharField('ФИО владельца', max_length=200, db_index=True)
+    owners_phonenumber = models.CharField('Номер владельца', max_length=20, db_index=True, null=True, blank=True)
+    owner_pure_phone = PhoneNumberField('Нормализованный номер владельца', db_index=True)
     created_at = models.DateTimeField(
         'Когда создано объявление',
         default=timezone.now,
         db_index=True)
 
-    description = models.TextField('Текст объявления', blank=True)
+    description = models.TextField('Текст объявления', blank=True, db_index=True)
     price = models.IntegerField('Цена квартиры', db_index=True)
 
     town = models.CharField(
@@ -22,14 +24,14 @@ class Flat(models.Model):
         'Район города, где находится квартира',
         max_length=50,
         blank=True,
-        help_text='Чертаново Южное')
+        help_text='Чертаново Южное', db_index=True)
     address = models.TextField(
         'Адрес квартиры',
-        help_text='ул. Подольских курсантов д.5 кв.4')
+        help_text='ул. Подольских курсантов д.5 кв.4', db_index=True)
     floor = models.CharField(
         'Этаж',
         max_length=3,
-        help_text='Первый этаж, последний этаж, пятый этаж')
+        help_text='Первый этаж, последний этаж, пятый этаж', db_index=True)
 
     rooms_number = models.IntegerField(
         'Количество комнат в квартире',
@@ -48,7 +50,7 @@ class Flat(models.Model):
         blank=True,
         db_index=True)
     new_building = models.BooleanField('Новостройка', null=True, blank=True, db_index=True)
-    likes = models.ManyToManyField(User, verbose_name='Кто лайкнул', blank=True)
+    likes = models.ManyToManyField(User, verbose_name='Кто лайкнул', blank=True, db_index=True)
 
     class Meta:
         verbose_name = 'Квартира'
